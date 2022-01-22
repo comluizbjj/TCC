cefb7824df706eff063f09b426799ba35361a96b
Luiz Henrique <comluizbjj@gmail.com>
2022-01-21 23:05:37 -0300

    Primeiro teste apresentando a tela principal

---
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        public HomeController(UserManager<MyUser> userManager )
+        {
+            _userManager = userManager;
+        }
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly UserManager<MyUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user == null)
+                {
+                    user = new MyUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName,
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Sucess");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp.Identity/MyUserStore.cs
@@ -0,0 +1,127 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Identity;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApp.Identity
+{
+    public class MyUserStore : IUserStore<MyUser>
+    {
+        public async Task<IdentityResult> CreateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbconnection())
+            {
+                await connection.ExecuteAsync(
+                    "insert into Users([Id]," +
+                    "[UserName]," +
+                    "[NormalizedUserName]," +
+                    "[PasswordHash]) " +
+                    "Values(@id,@username,@normalizedUserName,@passwordHash)",
+                   new
+                   {
+                       id = user.Id,
+                       userName = user.UserName,
+                       normalizeUserName = user.NormalizeUserName,
+                       passwordHash = user.PasswordHash,
+                   });
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbconnection())
+            {
+                await connection.ExecuteAsync("delete from Users where Id = @id",
+                   new
+                   {
+                       id = user.Id
+                   });
+            }
+            return IdentityResult.Success;
+        }
+
+        public void Dispose()
+        {
+           
+        }
+
+        public static DbConnection GetDbconnection()
+        {
+            var connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=master;Data Source=LAPTOP-7CO7RVCN\\SQLEXPRESS");
+            //var connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=master;Data Source=LAPTOP-7CO7RVCN\SQLEXPRESS");
+            connection.Open();
+            return connection;
+        }
+        public async Task<MyUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbconnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>(
+                    "select * from Users where Id = @id",
+                    new { id = userId });
+            }
+        }
+
+        public async Task<MyUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbconnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>(
+                    "select * from Users where normalizedUserName = @name",
+                    new { name = normalizedUserName });
+            }
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizeUserName);
+        }
+
+        public Task<string> GetUserIdAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task SetNormalizedUserNameAsync(MyUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.NormalizeUserName = normalizedName;
+            return Task.CompletedTask;
+        }
+
+        public Task SetUserNameAsync(MyUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.CompletedTask;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetDbconnection())
+            {
+                await connection.ExecuteAsync(
+                    "update Users " +
+                    "set [Id] = @id," +
+                    "[Username] = @userName," +
+                    "[NormalizedUsedName] = @normalizeUserName," +
+                    "[PasswordHash] = @passwordHash" +
+                    "where [Id] = @id",
+                   new
+                   {
+                       id = user.Id,
+                       userName = user.UserName,
+                       normalizeUserName = user.NormalizeUserName,
+                       passwordHash = user.PasswordHash,
+                   });
+            }
+            return IdentityResult.Success;
+        }
+    }
+}
